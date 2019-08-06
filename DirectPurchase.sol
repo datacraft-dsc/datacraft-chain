2ee0a6be2b2e1f099f3a85c98dbfb2a398acd322
Ilia <ilyabukalov@gmail.com>
2019-08-06 16:24:05 +0800

    correct log interface

--- DirectPurchase.sol
+++ DirectPurchase.sol
@@ -6,9 +6,9 @@ contract DirectPurchase {
 
 OceanToken token;
 
-    event Log(
-	address _from,
-	address _to,
+    event TokenSent(
+	address indexed _from,
+	address indexed _to,
 	uint256 _amount,
         bytes32 _reference1,
 	bytes32 _reference2
@@ -21,6 +21,6 @@ OceanToken token;
 
     function sendTokenAndLog(address to, uint256 amount, bytes32 reference1, bytes32 reference2) public {
 	token.transferFrom(msg.sender, to, amount);
-        emit Log(msg.sender, to, amount, reference1, reference2);
+        emit TokenSent(msg.sender, to, amount, reference1, reference2);
     }
 }
