5632335721353bb8f1c069e8824c613675e474d4
Ilia <ilyabukalov@gmail.com>
2019-08-08 14:09:19 +0800

    Python compilation compatibility with zeppelin package

--- compile.py
+++ compile.py
@@ -1,6 +1,7 @@
 import json
 from web3 import Web3
 from solcx import compile_files, link_code, compile_source
+import os
 # web3.py instance
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
@@ -16,10 +17,10 @@ def deploy_contract(contract_interface, *argv):
     tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
     return tx_receipt['contractAddress']
 
-# compile all contract files
-contracts = compile_files(['token.sol', 'DirectPurchase.sol'])
-token_contract = contracts.pop("token.sol:OceanToken")
-purchase_contract = contracts.pop("DirectPurchase.sol:DirectPurchase")
+zeppelin_remapping = 'openzeppelin-solidity=' + os.path.join(os.getcwd(), "node_modules/openzeppelin-solidity")
+contracts = compile_files(['contracts/OceanToken.sol', 'contracts/DirectPurchase.sol'], import_remappings=[zeppelin_remapping])
+token_contract = contracts.pop("contracts/OceanToken.sol:OceanToken")
+purchase_contract = contracts.pop("contracts/DirectPurchase.sol:DirectPurchase")
 token_address = deploy_contract(token_contract)
 print("Token contract address: ", token_address)
 purchase_address = deploy_contract(purchase_contract, token_address)
