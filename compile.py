18041a6f7d2169f1a5623e49d006eb00afc1e03f
Ilia <ilyabukalov@gmail.com>
2019-08-13 18:20:18 +0800

    parameter --artifacts added in python deployment script

--- compile.py
+++ compile.py
@@ -2,6 +2,11 @@ import json
 from web3 import Web3
 from solcx import compile_files, link_code, compile_source
 import os
+import sys
+import argparse
+parser = argparse.ArgumentParser(description='use --artifacts to upload contract on existing token')
+parser.add_argument('--artifacts', action='store_true', help='A boolean switch')
+args = parser.parse_args()
 # web3.py instance
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
@@ -19,9 +24,17 @@ def deploy_contract(contract_interface, *argv):
 
 zeppelin_remapping = 'openzeppelin-solidity=' + os.path.join(os.getcwd(), "node_modules/openzeppelin-solidity")
 contracts = compile_files(['contracts/OceanToken.sol', 'contracts/DirectPurchase.sol'], import_remappings=[zeppelin_remapping])
+
 token_contract = contracts.pop("contracts/OceanToken.sol:OceanToken")
 purchase_contract = contracts.pop("contracts/DirectPurchase.sol:DirectPurchase")
-token_address = deploy_contract(token_contract)
+
+if args.artifacts:
+    with open('artifacts/OceanToken.spree.json', 'r') as f:
+        token_artifact = json.load(f)
+
+    token_address = token_artifact['address']
+else:
+    token_address = deploy_contract(token_contract)
 print("Token contract address: ", token_address)
 purchase_address = deploy_contract(purchase_contract, token_address)
 print("Direct purchase contract address: ", purchase_address)
