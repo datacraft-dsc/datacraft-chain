2b942803f9890813d7918d29088a8403aa3446b6
Ilia <ilyabukalov@gmail.com>
2019-08-15 13:09:58 +0800

    migration script, deployment pipeline parameterized

--- migrations/2_purchase.js
+++ migrations/2_purchase.js
@@ -1,18 +1,32 @@
 const Purchase = artifacts.require("DirectPurchase");
-const Token = artifacts.require("OceanToken");
-var stdio = require('stdio');
+const stdio = require('stdio');
+
 var ops = stdio.getopt({
     'artifacts': {key: 'a', description: 'Take ABI from artifacts'},
     'reset': {key: 'r', description: ''},
     'compile-all': {key: 'c', description: ''}
 });
 
-module.exports = function(deployer) {
-    // Deploy the Token contract
-    deployer.deploy(Token)
-        // Wait until the storage contract is deployed
-        .then(() => Token.deployed())
+module.exports = async function(deployer) {
+	if(ops.artifacts) {
+            pipeline = deployer.then(() => {
+		const fs = require('fs');
+		const path = require("path");
+		var obj = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../artifacts/OceanToken.spree.json")));
+		token_address = obj.address;
+	    });
+	} else {
+	    const Token = artifacts.require("OceanToken");
+	    // Deploy the Token contract
+	    pipeline = deployer.deploy(Token)
+	    // Wait until the storage contract is deployed
+	    .then(() => Token.deployed())
+	    .then(() => token_address = Token.address);
+	}
+
         // Deploy the Purchase contract, while passing the address of the
         // Token contract
-        .then(() => deployer.deploy(Purchase, Token.address));
+        pipeline.then(() => deployer.deploy(Purchase, token_address))
+        .then(() => Purchase.deployed())
+        .then(() => console.log("Contract deployed with token address:", token_address));
 }
