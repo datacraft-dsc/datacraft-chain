1006d08c67ed12a563600ae3aaa4593b34db815f
Ilia Bukalov <ilyabukalov@gmail.com>
2020-04-06 17:39:15 +0800

    compose cmd updated

--- run_dex_chain_compose.sh
+++ run_dex_chain_compose.sh
@@ -1,8 +1,8 @@
 export PARITY_VERSION=v2.5.1
 export CONTRACTS_VERSION=v0.1
-export DEPLOY_CONTRACTS="true"
+export DEPLOY_CONTRACTS="false"
 export CHAIN_RPC_HOST="chain-node"
 export CHAIN_RPC_PORT="8545"
 export ARTIFACTS_FOLDER="./artifacts"
 
-docker-compose --project-name=dex -f ./compose-files/contracts.yml up --remove-orphans
+docker-compose --project-name=dex -f ./compose-files/contracts.yml -f ./compose-files/chain.yml up --remove-orphans
