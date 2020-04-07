dc71ccb5c6a0156f770a98308e0d6b7a09bd046e
Ilia Bukalov <ilyabukalov@gmail.com>
2020-04-07 17:47:23 +0800

    cmd for contracts fixed

--- patch.sh
+++ patch.sh
@@ -5,7 +5,7 @@ COMMAND_STATUS=1
 
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
 
-if [ "${LOCAL_CONTRACTS}" = "true" ]; then
+if [ "${WAIT_FOR_OCEAN_CONTRACTS}" = "true" ]; then
 	until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
 	    if [ -f "/usr/local/keeper-contracts/ready" ]; then
 	        COMMAND_STATUS=0
@@ -14,9 +14,11 @@ if [ "${LOCAL_CONTRACTS}" = "true" ]; then
 	    sleep 5
 	    (( RETRY_COUNT=RETRY_COUNT+1 ))
 	done
+fi
+
+if [ "${DEPLOY_DEX_CONTRACTS}" = "true" ]; then
 ./node_modules/.bin/openzeppelin push --network spree -d --skip-compile
 ./node_modules/.bin/truffle migrate --reset --compile-all --network spree
 touch "/usr/local/keeper-contracts/dex-ready"
-
 fi
 
--- run_dex_chain_compose.sh
+++ run_dex_chain_compose.sh
@@ -1,6 +1,6 @@
 export PARITY_VERSION=v2.5.1
 export CONTRACTS_VERSION=v0.1
-export DEPLOY_CONTRACTS="false"
+export DEPLOY_CONTRACTS="true"
 export CHAIN_RPC_HOST="chain-node"
 export CHAIN_RPC_PORT="8545"
 export ARTIFACTS_FOLDER="${PWD}/artifacts"
