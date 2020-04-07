9b10af428d8d9d5aae58460a19b568f479da9710
Ilia Bukalov <ilyabukalov@gmail.com>
2020-04-07 16:09:05 +0800

    bug fix. absolute path volume

--- run_dex_chain_compose.sh
+++ run_dex_chain_compose.sh
@@ -3,6 +3,7 @@ export CONTRACTS_VERSION=v0.1
 export DEPLOY_CONTRACTS="false"
 export CHAIN_RPC_HOST="chain-node"
 export CHAIN_RPC_PORT="8545"
-export ARTIFACTS_FOLDER="./artifacts"
+export ARTIFACTS_FOLDER="${PWD}/artifacts"
 
-docker-compose --project-name=dex -f ./compose-files/contracts.yml -f ./compose-files/chain.yml up --remove-orphans
+
+docker-compose --project-name=dex -f ./compose-files/contracts.yml -f ./compose-files/network.yml -f ./compose-files/chain.yml up --remove-orphans
