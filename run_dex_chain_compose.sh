4820686eb4317859c0985d9b612b10ebdb528577
Ilia Bukalov <ilyabukalov@gmail.com>
2020-04-08 15:27:16 +0800

    Dex chain version of contract. Token simulation

--- run_dex_chain_compose.sh
+++ run_dex_chain_compose.sh
@@ -1,5 +1,5 @@
 export PARITY_VERSION=v2.5.1
-export CONTRACTS_VERSION=v0.1
+export CONTRACTS_VERSION=v0.2
 export DEPLOY_CONTRACTS="true"
 export CHAIN_RPC_HOST="chain-node"
 export CHAIN_RPC_PORT="8545"
